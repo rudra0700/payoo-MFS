1a3b19f90a227c57bf1361940c76f233ecbed7c4
rudra0700 <baruarudra7@gmail.com>
2024-09-19 13:03:39 +0600

    home section added

--- js/login.js
+++ js/login.js
@@ -1,7 +1,14 @@
 document.getElementById("login-btn").addEventListener("click", (event) => {
-event.preventDefault();
+  event.preventDefault();
 
-const phoneNumber = document.getElementById("phone-number").value
-console.log(phoneNumber);
+  const phoneNumber = document.getElementById("phone-number").value;
+  const pinNumber = document.getElementById("pin-number").value;
+ 
+  
 
+  if (phoneNumber === "5" && pinNumber === "1234") {
+   window.location.href = "/home.html"
+  }else{
+    alert("wrong info")
+  }
 });
